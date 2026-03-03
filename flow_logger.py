21fffeb2ecd5472f3bd3137cdc81812e32da9e87
aman tamboli 07 <tamboliaman109@gmail.com>
2026-03-03 22:51:30 +0530

    added flow logger

--- flow_logger.py
+++ flow_logger.py
@@ -20,3 +20,15 @@ def get_logs():
 def clear_logs():
 
     FLOW_LOGS.clear()
+
+import time
+
+FLOW_LOGS = []
+
+
+def log_step(step, data=None):
+    FLOW_LOGS.append({
+        "step": step,
+        "data": str(data),
+        "timestamp": time.time()
+    })

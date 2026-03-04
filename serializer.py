646483c8a8a29bdd26fd31c18b16b87b0f55b707
aman tamboli 07 <tamboliaman109@gmail.com>
2026-03-04 22:52:34 +0530

    update in serializer

--- serializer.py
+++ serializer.py
@@ -10,3 +10,8 @@ class ProductSerializer(SerializerFlowMixin, serializers.Serializer):
     def create(self, validated_data):
 
         return validated_data
+
+class SerializerFlowLog(models.Model):
+    step = models.CharField(max_length=200)
+    data = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)

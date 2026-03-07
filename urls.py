ffbf2dd6f76b9e3d309f60c36fa0250862e3000c
aman tamboli 07 <tamboliaman109@gmail.com>
2026-03-07 22:26:36 +0530

    updated urls section

--- urls.py
+++ urls.py
@@ -1,10 +1,27 @@
-from django.urls import path,include
-from .views import SerializerTestView
+# from django.urls import path,include
+# from .views import SerializerTestView
 
+
+# urlpatterns = [
+
+#     path("serializer-flow/", SerializerTestView.as_view()),
+#     path("api/", include("flow.urls")),
+
+# ] 
+
+from django.urls import path
+from .views import (
+    SerializerTestView,
+    last_serializer_flow,
+    clear_serializer_logs
+)
 
 urlpatterns = [
 
     path("serializer-flow/", SerializerTestView.as_view()),
-    path("api/", include("flow.urls")),
+
+    path("serializer-flow-last/", last_serializer_flow),
+
+    path("serializer-flow-clear/", clear_serializer_logs),
 
 ]

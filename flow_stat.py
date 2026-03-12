9a3570ae8d59f59293c3a61eb8e97eeb550fa13e
aman tamboli 07 <tamboliaman109@gmail.com>
2026-03-12 23:45:48 +0530

    added flow_stat

---
+++ flow_stat.py
@@ -0,0 +1,16 @@
+from .flow_logger import get_logs
+
+def get_flow_stats():
+
+    logs = get_logs()
+
+    total_steps = len(logs)
+
+    success_count = sum(1 for l in logs if l["status"] == "SUCCESS")
+    error_count = sum(1 for l in logs if l["status"] == "ERROR")
+
+    return {
+        "total_steps": total_steps,
+        "success_steps": success_count,
+        "error_steps": error_count
+    }

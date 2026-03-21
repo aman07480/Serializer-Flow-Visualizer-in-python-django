8627cae72a58504ff7fc3f08e1082d31a5b786ec
aman tamboli 07 <tamboliaman109@gmail.com>
2026-03-21 23:27:35 +0530

    update in flow storage file

--- flow_storage.py
+++ flow_storage.py
@@ -1,15 +1,27 @@
-from django.db import models
+# from django.db import models
 
 
-class FlowLog(models.Model):
+# class FlowLog(models.Model):
 
-    step = models.CharField(max_length=200)
+#     step = models.CharField(max_length=200)
 
-    status = models.CharField(max_length=20)
+#     status = models.CharField(max_length=20)
 
-    data = models.TextField(null=True, blank=True)
+#     data = models.TextField(null=True, blank=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.step
+#     def __str__(self):
+#         return self.step
+
+from .flow_storage import save_flow_log
+
+def log_step(step, data=None, status="INFO"):
+
+    save_flow_log(step, status, data)
+
+    FLOW_LOGS.append({
+        "step": step,
+        "status": status,
+        "data": str(data)
+    })
